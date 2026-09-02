797fe63c860c732df6176ffaf2c324e3955e4de8
bsdtat2007-cloud <bsdtat2007@gmail.com>
2026-09-02 20:58:31 +0700

    Update emailjs.js

--- src/emailjs.js
+++ src/emailjs.js
@@ -3,6 +3,6 @@
 // Xem hướng dẫn lấy thông tin này trong file HUONG-DAN-DEPLOY.md, phần EmailJS.
 // EmailJS cho phép gửi email trực tiếp từ trình duyệt, miễn phí tới 200 email/tháng.
 // ==========================================================================
-export const EMAILJS_SERVICE_ID = 'service_lyos0ly';
+export const EMAILJS_SERVICE_ID = 'service_fmowyf6';
 export const EMAILJS_TEMPLATE_ID = 'template_4q88a1e';
 export const EMAILJS_PUBLIC_KEY = 'q-nk0ygS-DdRMgq3b';
